107bc0194d590b16916aeac7ff826c0950066f08
lkeix <ai.kanaria.ai@gmail.com>
2022-06-25 20:16:15 +0900

    initial commit

---
+++ src/tests/token_test.rs
@@ -0,0 +1,15 @@
+extern crate rstest;
+#[cfg(test)]
+extern crate speculate;
+
+use rstest::*;
+use speculate::speculate;
+use token::*;
+
+speculate! {
+  describe "Check constant tokens"  {
+    fn test_constant_token() {
+      assert_eq!(ILLEGAL, "";)
+    }
+  }
+}
---
+++ src/token/token.rs
@@ -0,0 +1,32 @@
+type Token i64
+
+pub const ILLEGAL: str = "ILLEGAL"
+pub const EOF: str = "EOF"
+pub const COMMENT: str = "COMMENT"
+
+pub const IDENT: str = "IDENT"
+pub const INT: str = "INT"
+pub const FLOAT: str = "FLOAT"
+pub const IMAG: str = "IMAG"
+pub const CHAR: str = "CHAR"
+pub const STRING: str = "STRING"
+
+pub const ADD: str = "+"
+pub const SUB: str = "-"
+pub const MUL: str = "*"
+pub const QUO: str = "/"
+pub const REM: str = "%"
+
+pub const ADD_ASSIGN: str = "+="
+pub const SUB_ASSIGN: str = "-="
+pub const MUL_ASSIGN: str = "*="
+pub const QUO_ASSIGN: str = "/="
+pub const REM_ASSIGN: str = "%="
+
+pub const AND_ASSIGN: str = "&="
+pub const OR_ASSIGN: str = "|="
+pub const XOR_ASSIGN: str = "^="
+pub const SHL_ASSIGN: str = "<<="
+pub const SHR_ASSIGN: str = ">>="
+pub const AND_NOT_ASSIGN: str = "&^="
+
