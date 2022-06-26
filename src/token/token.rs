e8e59761c666c7017b3e2e7b72bff4bbdf4b74e3
lkeix <ai.kanaria.ai@gmail.com>
2022-06-26 22:14:42 +0900

    add: necessary token identifier and token identifier test

--- src/token/token.rs
+++ src/token/token.rs
@@ -8,7 +8,7 @@ pub mod token_identifier {
   pub const FLOAT: &str = "FLOAT";
   pub const IMAG: &str = "IMAG";
   pub const CHAR: &str = "CHAR";
-  pub const STRING: &str = "&strING";
+  pub const STRING: &str = "STRING";
 
   pub const ADD: &str = "+";
   pub const SUB: &str = "-";
@@ -28,4 +28,65 @@ pub mod token_identifier {
   pub const SHL_ASSIGN: &str = "<<=";
   pub const SHR_ASSIGN: &str = ">>=";
   pub const AND_NOT_ASSIGN: &str = "&^=";
+
+  pub const LAND: &str = "&&";
+  pub const LOR: &str = "||";
+  pub const ARROW: &str = "<-";
+  pub const INC: &str = "++";
+  pub const DEC: &str = "--";
+
+  pub const EQL: &str = "==";
+  pub const LSS: &str = "<";
+  pub const GTR: &str = ">";
+  pub const ASSIGN: &str = "=";
+  pub const NOT: &str = "!";
+
+  pub const NEQ: &str = "!=";
+  pub const LEQ: &str = "<=";
+  pub const GEQ: &str = ">=";
+  pub const DEFINE: &str = ":=";
+  pub const ELLIPSIS: &str = "...";
+
+  pub const LPAREN: &str = "(";
+  pub const LBRACK: &str = "[";
+  pub const LBRACE: &str = "{";
+  pub const COMMA: &str = ",";
+  pub const PERIOD: &str = ".";
+
+  pub const RPAREN: &str = ")";
+  pub const RBRACK: &str = "]";
+  pub const RBRACE: &str = "}";
+  pub const SEMICOLON: &str = ";";
+  pub const COLON: &str = ":";
+
+  pub const BREAK: &str = "break";
+  pub const CASE: &str = "case";
+  pub const CHAN: &str = "chan";
+  pub const CONST: &str = "const";
+  pub const CONTINUE: &str = "continue";
+
+  pub const DEFAULE: &str = "default";
+  pub const DEFER: &str = "defer";
+  pub const ELSE: &str = "else";
+  pub const FALLTHROUGH: &str = "fallthrough";
+  pub const FOR: &str = "for";
+
+  pub const FUNC: &str = "func";
+  pub const GO: &str = "go";
+  pub const GOTO: &str = "goto";
+  pub const IF: &str = "if";
+  pub const IMPORT: &str = "import";
+
+  pub const INTERFACE: &str = "interface";
+  pub const MAP: &str = "map";
+  pub const PACKAGE: &str = "package";
+  pub const RANGE: &str = "range";
+  pub const RETURN: &str = "return";
+
+  pub const SELECT: &str = "select";
+  pub const STRUCT: &str = "struct";
+  pub const SWITCH: &str = "switch";
+  pub const TYPE: &str = "type";
+  pub const VAR: &str = "var";
+  pub const TILDE: &str = "~";
 }
--- tests/token_test.rs
+++ tests/token_test.rs
@@ -3,15 +3,102 @@ extern crate rstest;
 extern crate speculate;
 
 use rstest::*;
-use rtog;
+use rtog::token::token::token_identifier::*;
 use speculate::speculate;
 
 speculate! {
   describe "Check constant tokens"  {
     #[rstest]
     fn test_constant_token() {
-      assert_eq!(rtog::token::token::token_identifier::ILLEGAL, "ILLEGAL");
-      assert_eq!(rtog::token::token::token_identifier::EOF, "EOF");
+      assert_eq!(ILLEGAL, "ILLEGAL");
+      assert_eq!(EOF, "EOF");
+      assert_eq!(COMMENT, "COMMENT");
+      assert_eq!(IDENT, "IDENT");
+      assert_eq!(INT, "INT");
+      assert_eq!(FLOAT, "FLOAT");
+      assert_eq!(IMAG, "IMAG");
+      assert_eq!(CHAR, "CHAR");
+      assert_eq!(STRING, "STRING");
+
+      assert_eq!(ADD, "+");
+      assert_eq!(SUB, "-");
+      assert_eq!(MUL, "*");
+      assert_eq!(QUO, "/");
+      assert_eq!(REM, "%");
+
+      assert_eq!(ADD_ASSIGN, "+=");
+      assert_eq!(SUB_ASSIGN, "-=");
+      assert_eq!(MUL_ASSIGN, "*=");
+      assert_eq!(QUO_ASSIGN, "/=");
+      assert_eq!(REM_ASSIGN, "%=");
+
+      assert_eq!(AND_ASSIGN, "&=");
+      assert_eq!(OR_ASSIGN, "|=");
+      assert_eq!(XOR_ASSIGN, "^=");
+      assert_eq!(SHL_ASSIGN, "<<=");
+      assert_eq!(SHR_ASSIGN, ">>=");
+      assert_eq!(AND_NOT_ASSIGN, "&^=");
+
+      assert_eq!(LAND, "&&");
+      assert_eq!(LOR, "||");
+      assert_eq!(ARROW, "<-");
+      assert_eq!(INC, "++");
+      assert_eq!(DEC, "--");
+
+      assert_eq!(EQL, "==");
+      assert_eq!(LSS, "<");
+      assert_eq!(GTR, ">");
+      assert_eq!(ASSIGN, "=");
+      assert_eq!(NOT, "!");
+
+      assert_eq!(NEQ, "!=");
+      assert_eq!(LEQ, "<=");
+      assert_eq!(GEQ, ">=");
+      assert_eq!(DEFINE, ":=");
+      assert_eq!(ELLIPSIS, "...");
+
+      assert_eq!(LPAREN, "(");
+      assert_eq!(LBRACK, "[");
+      assert_eq!(LBRACE, "{");
+      assert_eq!(COMMA, ",");
+      assert_eq!(PERIOD, ".");
+
+      assert_eq!(RPAREN, ")");
+      assert_eq!(RBRACK, "]");
+      assert_eq!(RBRACE, "}");
+      assert_eq!(SEMICOLON, ";");
+      assert_eq!(COLON, ":");
+
+      assert_eq!(BREAK, "break");
+      assert_eq!(CASE, "case");
+      assert_eq!(CHAN, "chan");
+      assert_eq!(CONST, "const");
+      assert_eq!(CONTINUE, "continue");
+
+      assert_eq!(DEFAULE, "default");
+      assert_eq!(DEFER, "defer");
+      assert_eq!(ELSE, "else");
+      assert_eq!(FALLTHROUGH, "fallthrough");
+      assert_eq!(FOR, "for");
+
+      assert_eq!(FUNC, "func");
+      assert_eq!(GO, "go");
+      assert_eq!(GOTO, "goto");
+      assert_eq!(IF, "if");
+      assert_eq!(IMPORT, "import");
+
+      assert_eq!(INTERFACE, "interface");
+      assert_eq!(MAP, "map");
+      assert_eq!(PACKAGE, "package");
+      assert_eq!(RANGE, "range");
+      assert_eq!(RETURN, "return");
+
+      assert_eq!(SELECT, "select");
+      assert_eq!(STRUCT, "struct");
+      assert_eq!(SWITCH, "switch");
+      assert_eq!(TYPE, "type");
+      assert_eq!(VAR, "var");
+      assert_eq!(TILDE, "~");
     }
   }
 }
